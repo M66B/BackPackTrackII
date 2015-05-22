a2b08fa88856b3640c7530991b493b63e23906c0
M66B <marcel@faircode.eu>
2015-05-22 08:22:42 +0200

    Listen for location provider changes

---
+++ app/src/main/java/eu/faircode/backpacktrack2/ProvidersChangedReceiver.java
@@ -0,0 +1,19 @@
+package eu.faircode.backpacktrack2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+
+public class ProvidersChangedReceiver extends BroadcastReceiver {
+    private static final String TAG = "BPT2.ProvidersChangedReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.w(TAG, "Received " + intent);
+        LocationService.stopTracking(context);
+        LocationService.startTracking(context);
+    }
+}
