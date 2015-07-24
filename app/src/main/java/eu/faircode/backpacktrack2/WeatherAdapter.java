4eb3a5235f2acff25f645699339a42ac883828ca
M66B <marcel@faircode.eu>
2015-07-24 21:48:45 +0200

    Faster weather history

--- app/src/main/java/eu/faircode/backpacktrack2/WeatherAdapter.java
+++ app/src/main/java/eu/faircode/backpacktrack2/WeatherAdapter.java
@@ -17,6 +17,13 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class WeatherAdapter extends CursorAdapter {
+    private int colTime;
+    private int colTemperature;
+    private int colHumidity;
+    private int colPressure;
+    private int colWindSpeed;
+    private int colWindDirection;
+    private int colRain1h;
     private String temperature_unit;
     private String pressure_unit;
     private String speed_unit;
@@ -26,6 +33,13 @@ public class WeatherAdapter extends CursorAdapter {
 
     public WeatherAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
+        colTime = cursor.getColumnIndex("time");
+        colTemperature = cursor.getColumnIndex("temperature");
+        colHumidity = cursor.getColumnIndex("humidity");
+        colPressure = cursor.getColumnIndex("pressure");
+        colWindSpeed = cursor.getColumnIndex("wind_speed");
+        colWindDirection = cursor.getColumnIndex("wind_direction");
+        colRain1h = cursor.getColumnIndex("rain_1h");
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         temperature_unit = prefs.getString(SettingsFragment.PREF_TEMPERATURE, SettingsFragment.DEFAULT_TEMPERATURE);
         pressure_unit = prefs.getString(SettingsFragment.PREF_PRESSURE, SettingsFragment.DEFAULT_PRESSURE);
@@ -50,44 +64,46 @@ public class WeatherAdapter extends CursorAdapter {
         TextView tvPrecipitation = (TextView) view.findViewById(R.id.tvPrecipitation);
 
         // Time
-        long time = cursor.getLong(cursor.getColumnIndex("time"));
-        tvTime.setText(new SimpleDateFormat("dd").format(time) + " " + SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT).format(time));
+        long time = cursor.getLong(colTime);
+        tvTime.setText(
+                new SimpleDateFormat("dd").format(time) + " " +
+                        SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT).format(time));
 
         // Temperature
-        if (cursor.isNull(cursor.getColumnIndex("temperature")))
+        if (cursor.isNull(colTemperature))
             tvTemperature.setText("");
         else {
-            float temperature = cursor.getFloat(cursor.getColumnIndex("temperature"));
+            float temperature = cursor.getFloat(colTemperature);
             if ("f".equals(temperature_unit))
                 temperature = temperature * 9 / 5 + 32;
             tvTemperature.setText(DF.format(temperature));
         }
 
         // Humidity
-        if (cursor.isNull(cursor.getColumnIndex("humidity")))
+        if (cursor.isNull(colHumidity))
             tvHumidity.setText("");
         else {
-            int humidity = Math.round(cursor.getFloat(cursor.getColumnIndex("humidity")));
+            int humidity = Math.round(cursor.getFloat(colHumidity));
             if (humidity > 99)
                 humidity = 99;
             tvHumidity.setText(Long.toString(humidity));
         }
 
         // Pressure
-        if (cursor.isNull(cursor.getColumnIndex("pressure")))
+        if (cursor.isNull(colPressure))
             tvPressure.setText("");
         else {
-            float pressure = cursor.getFloat(cursor.getColumnIndex("pressure"));
+            float pressure = cursor.getFloat(colPressure);
             if ("mmhg".equals(pressure_unit))
                 pressure = pressure / 1.33322368f;
             tvPressure.setText(DF.format(pressure));
         }
 
         // Wind speed
-        if (cursor.isNull(cursor.getColumnIndex("wind_speed")))
+        if (cursor.isNull(colWindSpeed))
             tvWindSpeed.setText("");
         else {
-            float wind_speed = cursor.getFloat(cursor.getColumnIndex("wind_speed"));
+            float wind_speed = cursor.getFloat(colWindSpeed);
 
             if ("bft".equals(speed_unit))
                 wind_speed = (float) Math.pow(10.0, (Math.log10(wind_speed / 0.836) / 1.5));
@@ -101,19 +117,19 @@ public class WeatherAdapter extends CursorAdapter {
         }
 
         // Wind direction
-        if (cursor.isNull(cursor.getColumnIndex("wind_direction")))
+        if (cursor.isNull(colWindDirection))
             ivWindDirection.setVisibility(View.INVISIBLE);
         else {
-            float wind_direction = cursor.getFloat(cursor.getColumnIndex("wind_direction"));
+            float wind_direction = cursor.getFloat(colWindDirection);
             ivWindDirection.setRotation(wind_direction + 180);
             ivWindDirection.setVisibility(View.VISIBLE);
         }
 
         // Precipitation
-        if (cursor.isNull(cursor.getColumnIndex("rain_1h")))
+        if (cursor.isNull(colRain1h))
             tvPrecipitation.setText("");
         else {
-            float rain_1h = cursor.getFloat(cursor.getColumnIndex("rain_1h"));
+            float rain_1h = cursor.getFloat(colRain1h);
             if ("in".equals(rain_unit)) {
                 rain_1h = rain_1h / 25.4f;
                 tvPrecipitation.setText(DF2.format(rain_1h));
