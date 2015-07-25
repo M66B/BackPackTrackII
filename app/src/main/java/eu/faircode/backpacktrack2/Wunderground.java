fce415b15d5f86627766657039309b50997eb132
M66B <marcel@faircode.eu>
2015-07-25 17:31:34 +0200

    Wunderground

---
+++ app/src/main/java/eu/faircode/backpacktrack2/Wunderground.java
@@ -0,0 +1,8 @@
+package eu.faircode.backpacktrack2;
+
+public class Wunderground {
+    // http://www.wunderground.com/weather/api/d/docs?d=data/index
+    // http://api.wunderground.com/api/<API key>/conditions/pws:1/q/<lat>,<lon>.json
+    // http://api.wunderground.com/api/<API key>/geolookup/pws:1/q/<lat>,<lon>.json
+    // http://api.wunderground.com/api/<API key>/conditions/pws:1/q/pws:<station ID>.json
+}
