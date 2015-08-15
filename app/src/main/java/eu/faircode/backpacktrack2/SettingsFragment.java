6913056d0b518a055ae108b232aec51c3d53771f
M66B <marcel@faircode.eu>
2015-08-15 18:56:57 +0200

    Show no weather data

--- app/src/main/java/eu/faircode/backpacktrack2/SettingsFragment.java
+++ app/src/main/java/eu/faircode/backpacktrack2/SettingsFragment.java
@@ -2591,11 +2591,6 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
         boolean first = false;
         while (cursor.moveToNext()) {
-            if (cursor.isNull(colValue) &&
-                    (colValue2 < 0 ? true : cursor.isNull(colValue2)) &&
-                    (colValue3 < 0 ? true : cursor.isNull(colValue3)))
-                continue;
-
             data = true;
 
             long time = cursor.getLong(colTime);
@@ -2665,19 +2660,12 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         }
 
         if (data) {
-            long time = new Date().getTime();
-            if (time > maxTime) {
-                seriesValue.appendData(new DataPoint(new Date(time), Double.NaN), true, Integer.MAX_VALUE);
-                seriesValue2.appendData(new DataPoint(new Date(time), Double.NaN), true, Integer.MAX_VALUE);
-                seriesValue3.appendData(new DataPoint(new Date(time), Double.NaN), true, Integer.MAX_VALUE);
-            }
-
             graph.removeAllSeries();
             graph.getSecondScale().getSeries().clear();
 
             graph.getViewport().setXAxisBoundsManual(true);
-            graph.getViewport().setMinX(time - viewport);
-            graph.getViewport().setMaxX(time);
+            graph.getViewport().setMinX(maxTime - viewport);
+            graph.getViewport().setMaxX(maxTime);
 
             graph.getViewport().setYAxisBoundsManual(true);
             graph.getViewport().setMinY(Math.min(minValue, colValue2 < 0 ? minValue : minValue2));
