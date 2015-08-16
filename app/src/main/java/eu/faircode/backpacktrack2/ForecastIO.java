cc3c9af7f3b6a7e386527b5dbbea6d5a0c61769a
M66B <marcel@faircode.eu>
2015-08-16 14:55:57 +0200

    Refactoring

--- app/src/main/java/eu/faircode/backpacktrack2/ForecastIO.java
+++ app/src/main/java/eu/faircode/backpacktrack2/ForecastIO.java
@@ -2,6 +2,7 @@ package eu.faircode.backpacktrack2;
 
 import android.content.Context;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -76,37 +77,42 @@ public class ForecastIO {
             if (!currently.has("time"))
                 return null;
 
-            Weather weather = new Weather();
-            weather.time = currently.getLong("time") * 1000;
-            weather.provider = "fio";
-            weather.station_id = -1;
-            weather.station_type = -1;
-            weather.station_name = "-";
-
-            Location station_location = new Location("station");
-            station_location.setLatitude(jroot.getDouble("latitude"));
-            station_location.setLongitude(jroot.getDouble("longitude"));
-            weather.station_location = station_location;
-
-            weather.temperature = (currently.has("temperature") ? currently.getDouble("temperature") : Double.NaN);
-            weather.humidity = (currently.has("humidity") ? currently.getDouble("humidity") * 100 : Double.NaN);
-            weather.pressure = (currently.has("pressure") ? currently.getDouble("pressure") : Double.NaN);
-            weather.wind_speed = (currently.has("windSpeed") ? currently.getDouble("windSpeed") : Double.NaN);
-            weather.wind_gust = Double.NaN;
-            weather.wind_direction = (currently.has("windBearing") ? currently.getDouble("windBearing") : Double.NaN);
-            weather.visibility = (currently.has("visibility") ? currently.getDouble("visibility") * 1000 : Double.NaN);
-            weather.rain_1h = (currently.has("precipIntensity") ? currently.getDouble("precipIntensity") : Double.NaN);
-            weather.rain_today = (currently.has("precipAccumulation") ? currently.getDouble("precipAccumulation") * 10 : Double.NaN);
-            weather.rain_probability = (currently.has("precipProbability") ? currently.getDouble("precipProbability") * 100 : Double.NaN);
-            weather.clouds = (currently.has("cloudCover") ? currently.getDouble("cloudCover") * 100 : Double.NaN);
-            weather.icon = (currently.has("icon") ? currently.getString("icon") : null);
-            // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
-            weather.summary = (currently.has("summary") ? currently.getString("summary") : null);
-            weather.rawData = currently.toString();
-
-            return weather;
+            return decodeWeather(jroot, currently);
         } finally {
             urlConnection.disconnect();
         }
+    }
+
+    @NonNull
+    private static Weather decodeWeather(JSONObject jroot, JSONObject currently) throws JSONException {
+        Weather weather = new Weather();
+        weather.time = currently.getLong("time") * 1000;
+        weather.provider = "fio";
+        weather.station_id = -1;
+        weather.station_type = -1;
+        weather.station_name = "-";
+
+        Location station_location = new Location("station");
+        station_location.setLatitude(jroot.getDouble("latitude"));
+        station_location.setLongitude(jroot.getDouble("longitude"));
+        weather.station_location = station_location;
+
+        weather.temperature = (currently.has("temperature") ? currently.getDouble("temperature") : Double.NaN);
+        weather.humidity = (currently.has("humidity") ? currently.getDouble("humidity") * 100 : Double.NaN);
+        weather.pressure = (currently.has("pressure") ? currently.getDouble("pressure") : Double.NaN);
+        weather.wind_speed = (currently.has("windSpeed") ? currently.getDouble("windSpeed") : Double.NaN);
+        weather.wind_gust = Double.NaN;
+        weather.wind_direction = (currently.has("windBearing") ? currently.getDouble("windBearing") : Double.NaN);
+        weather.visibility = (currently.has("visibility") ? currently.getDouble("visibility") * 1000 : Double.NaN);
+        weather.rain_1h = (currently.has("precipIntensity") ? currently.getDouble("precipIntensity") : Double.NaN);
+        weather.rain_today = (currently.has("precipAccumulation") ? currently.getDouble("precipAccumulation") * 10 : Double.NaN);
+        weather.rain_probability = (currently.has("precipProbability") ? currently.getDouble("precipProbability") * 100 : Double.NaN);
+        weather.clouds = (currently.has("cloudCover") ? currently.getDouble("cloudCover") * 100 : Double.NaN);
+        weather.icon = (currently.has("icon") ? currently.getString("icon") : null);
+        // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+        weather.summary = (currently.has("summary") ? currently.getString("summary") : null);
+        weather.rawData = currently.toString();
+
+        return weather;
     }
 }
