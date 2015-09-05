802c9bd4e180fc0bc0b69e7a0eb4bdc02e593a3f
M66B <marcel@faircode.eu>
2015-09-05 10:07:08 +0200

    Added power save mode changed receiver to update weather when coming from idle mode

---
+++ app/src/main/java/eu/faircode/backpacktrack2/PowerSaveModeChangedReceiver.java
@@ -0,0 +1,37 @@
+package eu.faircode.backpacktrack2;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.PowerManager;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.util.Date;
+
+@TargetApi(Build.VERSION_CODES.M)
+public class PowerSaveModeChangedReceiver extends BroadcastReceiver {
+    private static final String TAG = "BPT2.PowerSave";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.i(TAG, "Received " + intent);
+
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (!pm.isDeviceIdleMode() && Util.isConnected(context)) {
+            long time = new Date().getTime();
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            long ref_time = prefs.getLong(SettingsFragment.PREF_PRESSURE_REF_TIME, 0);
+            int interval = Integer.parseInt(prefs.getString(SettingsFragment.PREF_WEATHER_INTERVAL, SettingsFragment.DEFAULT_WEATHER_INTERVAL));
+
+            if (ref_time + interval * 60 * 1000 < time) {
+                Intent intentWeather = new Intent(context, BackgroundService.class);
+                intentWeather.setAction(BackgroundService.ACTION_UPDATE_WEATHER);
+                context.startService(intentWeather);
+            }
+        }
+    }
+}
