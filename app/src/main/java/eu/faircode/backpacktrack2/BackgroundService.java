7333f0932a209cf2380e65ee3bab08c7694048e2
M66B <marcel@faircode.eu>
2015-09-09 07:58:27 +0200

    Added forecast action to weather notification

--- app/src/main/java/eu/faircode/backpacktrack2/BackgroundService.java
+++ app/src/main/java/eu/faircode/backpacktrack2/BackgroundService.java
@@ -147,9 +147,11 @@ public class BackgroundService extends IntentService {
     public static final int REQUEST_STOP = 4;
     public static final int REQUEST_TIMEOUT = 5;
     public static final int REQUEST_WEATHER = 6;
-    public static final int REQUEST_RAIN = 7;
-    public static final int REQUEST_STEPS = 8;
-    public static final int REQUEST_RESTART = 9;
+    public static final int REQUEST_REFRESH = 7;
+    public static final int REQUEST_FORECAST = 8;
+    public static final int REQUEST_RAIN = 9;
+    public static final int REQUEST_STEPS = 10;
+    public static final int REQUEST_RESTART = 11;
 
     private static int mEGM96Pointer = -1;
     private static int mEGM96Offset;
@@ -1774,16 +1776,24 @@ public class BackgroundService extends IntentService {
         PendingIntent piMain = PendingIntent.getActivity(context, REQUEST_WEATHER, riMain, PendingIntent.FLAG_CANCEL_CURRENT);
         notificationBuilder.setContentIntent(piMain);
 
-        // Build update intent
-        Intent riUpdate = new Intent(context, BackgroundService.class);
-        riUpdate.setAction(BackgroundService.ACTION_UPDATE_WEATHER);
-        PendingIntent piUpdate = PendingIntent.getService(context, REQUEST_TRACKPOINT, riUpdate, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Build refresh intent
+        Intent riRefresh = new Intent(context, BackgroundService.class);
+        riRefresh.setAction(BackgroundService.ACTION_UPDATE_WEATHER);
+        PendingIntent piRefresh = PendingIntent.getService(context, REQUEST_REFRESH, riRefresh, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Build forecast intent
+        Intent riForecast = new Intent(context, SettingsActivity.class);
+        riForecast.putExtra(SettingsFragment.EXTRA_ACTION, SettingsFragment.ACTION_FORECAST);
+        PendingIntent piForecast = PendingIntent.getActivity(context, REQUEST_FORECAST, riForecast, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Add action
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            notificationBuilder.addAction(new Notification.Action.Builder(Icon.createWithResource(context, R.drawable.refresh_black), context.getString(R.string.title_refresh), piUpdate).build());
-        else
-            notificationBuilder.addAction(android.R.drawable.ic_menu_add, context.getString(R.string.title_refresh), piUpdate);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            notificationBuilder.addAction(new Notification.Action.Builder(Icon.createWithResource(context, R.drawable.refresh_black), context.getString(R.string.title_refresh), piRefresh).build());
+            notificationBuilder.addAction(new Notification.Action.Builder(Icon.createWithResource(context, R.drawable.cloud_download_black), context.getString(R.string.title_forecast), piForecast).build());
+        } else {
+            notificationBuilder.addAction(R.drawable.refresh_black, context.getString(R.string.title_refresh), piRefresh);
+            notificationBuilder.addAction(R.drawable.cloud_download_black, context.getString(R.string.title_forecast), piForecast);
+        }
 
         notificationBuilder.setUsesChronometer(true);
         notificationBuilder.setWhen(weather.time);
